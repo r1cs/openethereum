a3cb397361b87d2e70d15d3fd6da721c9f5417ad
r1cs <1027309561@qq.com>
2022-03-25 18:27:58 +0800

    trace: rm std

--- crates/ethcore/src/trace/types/filter.rs
+++ crates/ethcore/src/trace/types/filter.rs
@@ -17,8 +17,8 @@
 //! Trace filters type definitions
 
 use super::trace::{Action, Res};
+use core::ops::Range;
 use ethereum_types::{Address, Bloom, BloomInput};
-use std::ops::Range;
 use trace::flat::FlatTrace;
 
 /// Addresses filter.
